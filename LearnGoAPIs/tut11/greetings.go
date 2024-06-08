47ad2f0aaf25d67d615a709e0099fcd0fa6b0813
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-06-08 15:17:04 +0530

    c

---
+++ LearnGoAPIs/tut11/greetings.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var points = []int{1, 2, 3, 4, 5, 6}
+
+func SayHello(n string) {
+	fmt.Println("hello", n)
+}
+
+func showScore() {
+	fmt.Println("you scored", score)
+}
